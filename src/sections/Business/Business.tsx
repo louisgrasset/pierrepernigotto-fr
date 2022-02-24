ad6c8bcfe159d8314f06e4f5e23d6e7c4e1de1b4
Louis Grasset <louisgrasset@protonmail.com>
2022-02-24 15:22:28 +0100

    fix: remove text

--- src/sections/Business/Business.tsx
+++ src/sections/Business/Business.tsx
@@ -6,7 +6,7 @@ export function Business () {
     return (
         <section className="section-business" id="formation">
             <Header content="Entreprenariat" />
-            <p>Dans ma vie personnelle je suis gérant de deux SCI dont les objectifs sont la rénovation et la location de biens immobiliers.<br/> Je suis également auteur d'un livre.</p>
+            <p>Dans ma vie personnelle je suis gérant de deux SCI dont les objectifs sont la rénovation et la location de biens immobiliers.</p>
         </section>
     );
 }
