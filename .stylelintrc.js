76649ce4d4b11ae13aa4ab9b9cc76895cdd7a0c8
Louis Grasset <louisgrasset@protonmail.com>
2024-07-13 18:19:41 +0200

    chore(config): migrate for css nesting support; enhanced support

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,17 +1,14 @@
 module.exports = {
-    extends: [
-        "stylelint-config-standard",
-        "stylelint-config-sass-guidelines"
-    ],
-    defaultSeverity: "warning",
-    rules: {
-        "at-rule-no-unknown": null,
-        "indentation": 4,
-        "max-nesting-depth": 5,
-        "scss/at-rule-no-unknown": null,
-        "selector-class-pattern": "^[a-z][a-z0-9\\-_]*[a-z0-9]$",
-        "selector-max-compound-selectors": 5,
-        "no-invalid-position-at-import-rule": null,
-        "color-function-notation": "legacy"
-    }
-}
+  extends: ["stylelint-config-sass-guidelines"],
+  defaultSeverity: "warning",
+  rules: {
+    "at-rule-no-unknown": null,
+    "max-nesting-depth": 6,
+    "scss/at-rule-no-unknown": null,
+    "selector-class-pattern": "^[a-z][a-z0-9\\-_]*[a-z0-9]$",
+    "selector-max-compound-selectors": 5,
+    "no-invalid-position-at-import-rule": null,
+    "function-no-unknown": true,
+    "color-function-notation": "legacy",
+  },
+};
