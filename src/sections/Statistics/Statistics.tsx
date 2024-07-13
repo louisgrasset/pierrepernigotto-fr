165f4d58e6102ca42a4c5836b1f5187816022af7
Louis Grasset <louisgrasset@protonmail.com>
2024-07-13 20:05:39 +0200

    chore(config): reformat without prettier

--- src/sections/Statistics/Statistics.tsx
+++ src/sections/Statistics/Statistics.tsx
@@ -5,77 +5,77 @@ import CountUp from "react-countup";
 
 import { Information } from "../../components";
 import {
-  bacPlus2Projects,
-  bacPlus3Projects,
-  bacPlus5Projects,
-  endOfStudyProjects,
-  skills,
+    bacPlus2Projects,
+    bacPlus3Projects,
+    bacPlus5Projects,
+    endOfStudyProjects,
+    skills,
 } from "../../data";
 
 export function Statistics() {
-  const projectsCount = useMemo(
-    () =>
-      bacPlus2Projects.length +
+    const projectsCount = useMemo(
+        () =>
+            bacPlus2Projects.length +
       bacPlus3Projects.length +
       bacPlus5Projects.length +
       endOfStudyProjects.length,
-    [],
-  );
-  const experience = useMemo(() => new Date().getFullYear() - 2018, []);
-  const hardskills = useMemo(() => skills.map((skill) => skill.name), []);
-  const softskills = [
-    "Proactif",
-    "Organisé",
-    "Polyvalent",
-    "Autodidacte",
-    "Passionné",
-    "Attentif",
-    "Autonome",
-  ];
+        [],
+    );
+    const experience = useMemo(() => new Date().getFullYear() - 2018, []);
+    const hardskills = useMemo(() => skills.map((skill) => skill.name), []);
+    const softskills = [
+        "Proactif",
+        "Organisé",
+        "Polyvalent",
+        "Autodidacte",
+        "Passionné",
+        "Attentif",
+        "Autonome",
+    ];
 
-  return (
-    <section className="section-statistics">
-      <ul className="section-statistics__list">
-        <li className="section-statistics__list-item">
-          <span className="section-statistics__list-item__count">
+    return (
+        <section className="section-statistics">
+            <ul className="section-statistics__list">
+                <li className="section-statistics__list-item">
+                    <span className="section-statistics__list-item__count">
             +<CountUp start={1} end={experience} duration={0.4} />
-          </span>
+                    </span>
           Années d'experience
-        </li>
-        <li className="section-statistics__list-item">
-          <span className="section-statistics__list-item__count">
+                </li>
+                <li className="section-statistics__list-item">
+                    <span className="section-statistics__list-item__count">
             +<CountUp end={projectsCount} duration={1} />
-          </span>
+                    </span>
           Projets réalisés
-        </li>
-        <li
-          className="section-statistics__list-item section-statistics__list-item--separator"
-          aria-hidden
-        >
+                </li>
+                <li
+                    className="section-statistics__list-item section-statistics__list-item--separator"
+                    aria-hidden
+                >
           |
-        </li>
-        <li className="section-statistics__list-item">
-          <span className="section-statistics__list-item__count">
+                </li>
+                <li className="section-statistics__list-item">
+                    <span className="section-statistics__list-item__count">
             Hardskills
-          </span>
+                    </span>
           + <CountUp end={hardskills.length} duration={1} /> technologies
           maitrisées
-          <Information content={hardskills.join(", ") + "..."} />
-        </li>
-        <li
-          className="section-statistics__list-item section-statistics__list-item--separator"
-          aria-hidden
-        >
+                    <Information content={hardskills.join(", ") + "..."} />
+                </li>
+                <li
+                    className="section-statistics__list-item section-statistics__list-item--separator"
+                    aria-hidden
+                >
           |
-        </li>
-        <li className="section-statistics__list-item">
-          <span className="section-statistics__list-item__count">
+                </li>
+                <li className="section-statistics__list-item">
+                    <span className="section-statistics__list-item__count">
             Softskills
-          </span>
+                    </span>
           + <CountUp end={softskills.length} duration={1} /> qualités
-          <Information content={softskills.join(", ") + "..."} />
-        </li>
-      </ul>
-    </section>
-  );
+                    <Information content={softskills.join(", ") + "..."} />
+                </li>
+            </ul>
+        </section>
+    );
 }
